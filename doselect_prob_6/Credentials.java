3a026e9b54457357758bfc5f129f7fefd085a297
ArunaE17 <93649537+ArunaE17@users.noreply.github.com>
2022-04-21 16:44:10 +0530

    Add files via upload

---
+++ doselect_prob_6/Credentials.java
@@ -0,0 +1,14 @@
+package doselect_prob_6;
+
+public class Credentials {
+	String userName;
+	String password;
+public Credentials(String userName,String password)
+{
+	this.userName = userName;
+	this.password=password;
+	
+}
+}
+
+
---
+++ doselect_prob_6/LengthMismatchException.java
@@ -0,0 +1,8 @@
+package doselect_prob_6;
+
+public class LengthMismatchException extends Exception{
+	public LengthMismatchException(String str) {
+		super(str);
+	}
+
+}

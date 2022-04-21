86f954d4a38c8f0954bfc7ddf5225e1297394452
ArunaE17 <93649537+ArunaE17@users.noreply.github.com>
2022-04-21 16:46:23 +0530

    Delete doselect_prob_6 directory

--- doselect_prob_6/LengthMismatchException.java
+++
@@ -1,8 +0,0 @@
-package doselect_prob_6;
-
-public class LengthMismatchException extends Exception{
-	public LengthMismatchException(String str) {
-		super(str);
-	}
-
-}
--- doselect_prob_6/MissingCharacterException.java
+++
@@ -1,8 +0,0 @@
-package doselect_prob_6;
-
-public class MissingCharacterException extends Exception{
-	public MissingCharacterException(String str) {
-		super(str);
-	}
-
-}
